e750b110f034eb6063bbcb8f9a6f418d89fad9d4
ASROWE <asrowe@gmail.com>
2017-01-12 20:25:33 +0000

    first game of life complete

--- src/piworld.py
+++ src/piworld.py
@@ -6,9 +6,9 @@ class PiWorld:
     def __init__(self):
         self.sense = SenseHat()
         self.world = []
-        self.x = 8
-        self.y = 8
-        self.w = self.x * self.y
+        self.X = 8
+        self.Y = 8
+        self.W = self.X * self.Y
 
     def __str__(self):
         return str(self.world)
@@ -16,45 +16,79 @@ class PiWorld:
     def hattest(self):
         self.sense.show_message("Hi!")
 
-    def create(self):
-        for i in range(0,self.w):
-            self.world.append(i % 2 == 3)
+    def create(self, map = "checks"):
+        if map == "checks":
+            for y in range(0, self.Y):
+                self.world.append([1,0,1,0,1,0,1,0] if y % 2 == 0 else [0,1,0,1,0,1,0,1])
+        elif map == "blinker":
+            self.world = [[0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,1,1,1,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0,0]]
+        
+
         self.draw()
 
     def iterate(self, iterations):
-        time.sleep(0.5)
+       
         for i in range(0, iterations):
-            
-            newworld = [self.updateCell(j) for j in range(0,self.w)]
+            time.sleep(0.5)
+            new_world = []
+            for y in range(0, self.Y):
+                new_y = []
+                for x in range(0, self.X):
+                    new_y.append(self.updateCell((x,y)))
+                new_world.append(new_y)
+            self.world = new_world
             self.draw()
-            #print("World " + str(i) + " " + str(self.world))
 
     def updateCell(self, cell):
         neighbours = self.getNeighbours(cell)
-        self.world[cell] = self.updateState(cell, neighbours)
+        return self.updateState(cell, neighbours)
                 
-    def getNeighbours(self, cell):
-        neighbours_offset = [-9,-8,-7,-1,1,7,8,9]
-        neighbours_offset = [cell+i for i in neighbours_offset]
+    def getNeighbours(self, (x,y)):
+        neighbours_offset = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
+        neighbours_offset = [(x+o_x, y+o_y) for (o_x, o_y) in neighbours_offset]
+        neighbours_offset = [(x % self.X, y % self.Y) for (x, y) in neighbours_offset]
+
         neighbours = []
-        for i in neighbours_offset:
-            if i < 0 or i > self.w:
-                neighbours.append(True)
-            else:
-                neighbours.append(self.world[i])
-        #print(str(cell), str(neighbours_offset), str(neighbours) )   
+        for x,y in neighbours_offset:
+            neighbours.append(self.world[y][x])
+
         return neighbours
 
-    def updateState(self, cell, neighbours):
-        return not(self.world[cell])
+    def updateState(self, (x,y), neighbours):
+        neighbour_count = sum(neighbours)
+        
+        if self.world[y][x] == 1:
+            if neighbour_count < 2 or neighbour_count > 3:
+                new_val = 0
+            else:
+                new_val = 1
+        else:
+            if neighbour_count == 3:
+                new_val = 1
+            else:
+                new_val = 0
+                    
+        #print new_val
+        return new_val
     
     def draw(self):
-        pixels = [[255,255,255]if i else [0,0,0] for i in self.world]
+        pixels = []
+        for y in self.world:
+            for x in y:
+                pixels.append([255,255,255]if x == 1 else [0,0,0])
         self.sense.set_pixels(pixels)
 
 
 pworld = PiWorld()
 #pworld.hattest()
-pworld.create()
-pworld.iterate(1)
+pworld.create("blinker")
+pworld.iterate(10)
+print("finished")
         
